88558a8c11c03949740849acbaf456990c56210e
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2023-04-29 19:48:27 +0200

    Small refactoring.

--- src/main/java/com/mout/producer/KafkaProducerFactory.java
+++ src/main/java/com/mout/producer/KafkaProducerFactory.java
@@ -9,7 +9,7 @@ import static org.apache.kafka.clients.producer.ProducerConfig.*;
 class KafkaProducerFactory {
 
     KafkaProducer<Void, Integer> create() {
-        Properties props = new Properties();
+        var props = new Properties();
         props.setProperty(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.setProperty(KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.VoidSerializer");
         props.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerSerializer");
